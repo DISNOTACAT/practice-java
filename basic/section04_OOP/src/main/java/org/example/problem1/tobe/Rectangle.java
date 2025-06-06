e30d078357fb36036aee12ce0f72dcb0ec5e6d6f
DISNOTACAT <yeonjh915@gmail.com>
2025-06-06 16:13:08 +0900

    section04: OOP

---
+++ basic/section04_OOP/src/main/java/org/example/problem1/tobe/Rectangle.java
@@ -0,0 +1,24 @@
+package org.example.problem1.tobe;
+
+public class Rectangle {
+
+  int width;
+  int height;
+
+  public Rectangle(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public int calculateArea() {
+    return this.width * this.height;
+  }
+
+  public int calculatePerimeter() {
+    return 2 * (this.width + this.height);
+  }
+
+  public boolean isSquare() {
+    return this.width == this.height;
+  }
+}
